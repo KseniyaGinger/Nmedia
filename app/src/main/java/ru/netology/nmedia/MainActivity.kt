eddbcd6c9267b6086ba35a589ef42858f997db38
Kseniya Ginger <mega_kalina@mail.ru>
2023-09-22 17:56:24 +0300

    format count

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -27,8 +27,9 @@ class MainActivity : AppCompatActivity() {
             published = "21 мая в 18:36",
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
             likedByMe = true,
-            likes = 90,
-            shared = 20
+            likes = 1000,
+            shared = 1200,
+            views = 5000000
         )
 
         with(binding) {
@@ -37,6 +38,9 @@ class MainActivity : AppCompatActivity() {
             published.text = post.published
             likes.text = post.likes.toString()
             shared.text = post.shared.toString()
+            likes.text = formatCount(post.likes)
+            shared.text = formatCount(post.shared)
+            views.text = formatCount(post.views)
 
             if (post.likedByMe) {
                 like.setImageResource(R.drawable.baseline_favorite_24)
@@ -47,15 +51,29 @@ class MainActivity : AppCompatActivity() {
                 post.likes += if (post.likedByMe) 1 else -1
                 like.setImageResource(if (post.likedByMe) (R.drawable.baseline_favorite_24) else R.drawable.baseline_favorite_border_24)
                 likes.text = post.likes.toString()
+                likes.text = formatCount(post.likes)
             }
 
             share.setOnClickListener{
                 post.shared += 1
                 shared.text = post.shared.toString()
+                shared.text = formatCount(post.shared)
             }
 
         }
 
     }
+
+        fun formatCount(count: Int): String {
+            return when {
+                count < 1000 -> count.toString()
+                count < 10000 -> "${count / 1000}K"
+                count < 1000000 -> {
+                    val decimal = count % 1000 / 100
+                    "${count / 1000}.$decimal" + "K"
+                }
+                else -> "${count / 1000000}M"
+            }
+        }
 
 }
